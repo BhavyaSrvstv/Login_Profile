71c71c25eef1b6197dd9ada50387f82e43364492
Bhavya Srivastava <111657208+BhavyaSrvstv@users.noreply.github.com>
2023-07-19 17:19:09 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import redis
 
